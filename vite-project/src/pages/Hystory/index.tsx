e28ba737da685fe09be53b1679d01f928225aebd
MathMendesReis <matheusmendesreis@outlook.com>
2023-08-03 13:03:30 -0300

    estrutura html do componente pronta

--- vite-project/src/pages/Hystory/index.tsx
+++ vite-project/src/pages/Hystory/index.tsx
@@ -2,7 +2,25 @@
 export default function Hystory() {
     return (
    <div>
-    <h1>hystory</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Tarefa</th>
+          <th>Duração</th>
+          <th>Inicio</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>tarefa</td>
+          <td>duração</td>
+          <td>inicio</td>
+          <td>status</td>
+        </tr>
+      </tbody>
+    </table>
    </div>
     )
 }
