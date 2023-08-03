6f2b30604827dd1c6c1d54f9bc723161fd0812ef
MathMendesReis <matheusmendesreis@outlook.com>
2023-08-03 09:37:57 -0300

    assets e tematizacao concluida

--- vite-project/src/router/useRouter.tsx
+++ vite-project/src/router/useRouter.tsx
@@ -2,11 +2,18 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Main from "../pages/Main";
+import { DefaultLayouts } from "../layouts/defaultLayout";
 export default function useRouter() {
     const router = createBrowserRouter([
         {
           path: "/",
-          element: <Main/>,
+          element: <DefaultLayouts/>,
+          children: [
+            {
+              path:"/",
+              element: <Main />,
+            }
+          ]
         },
       ]);
       return [router];
