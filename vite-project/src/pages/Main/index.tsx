9efbf503bd758f1e3d8666bda8dcede3dd623fa6
MathMendesReis <matheusmendesreis@outlook.com>
2023-08-04 10:54:28 -0300

    formulario validado e funciionando

--- vite-project/src/pages/Main/index.tsx
+++ vite-project/src/pages/Main/index.tsx
@@ -1,12 +1,33 @@
 import { CountDown, FormContainer, HomeContainer, MinutsInput, Separator, StartCount, TaskInput } from "./styled"
 import { Play } from "@phosphor-icons/react";
 import { useForm } from 'react-hook-form'
+import { zodResolver} from '@hookform/resolvers/zod'
+import  {z}  from 'zod'
+
+
+const newTaskSchemma = z.object({
+  task:z.string().min(1, 'Informe a tarefa'),
+  minutsAmmout:z.number().min(5).max(60)
+})
+
+
+
 
 export default function Main() {
-  const {register, handleSubmit, watch} = useForm()
+  const {register, handleSubmit, watch, reset, formState:{errors}} = useForm<newTask>({
+    resolver: zodResolver(newTaskSchemma),
+    defaultValues:{
+      task:'',
+      minutsAmmout:0
+    }
+  })
 
-  function handleNewSubmit(data:any) {
+
+  type newTask = z.infer<typeof newTaskSchemma>
+
+  function handleNewSubmit(data:newTask) {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
@@ -34,7 +55,7 @@ export default function Main() {
         <label
           htmlFor="minutesAmmount"> durante
           <MinutsInput
-          {...register('minutsAmmout')}
+          {...register('minutsAmmout', {valueAsNumber: true})}
            type="number"
            placeholder="00"
            size={5}
