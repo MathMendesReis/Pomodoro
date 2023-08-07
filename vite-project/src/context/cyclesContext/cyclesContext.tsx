7b95005547742c8d596fff507ba775578162ec24
MathMendesReis <matheusmendesreis@outlook.com>
2023-08-07 12:17:18 -0300

    reduce criado e funcionaod

--- vite-project/src/context/cyclesContext/cyclesContext.tsx
+++ vite-project/src/context/cyclesContext/cyclesContext.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useReducer, useState } from "react";
+import { newTask } from "../../pages/Main";
 
 export interface Cycle {
   id:string
@@ -12,13 +13,15 @@ export interface Cycle {
 
 interface CyclesContextType {
   activeCycle: Cycle | undefined
-  isActive: string | null
+  activeCycleId: string | null
   ammoutSeconds:number
   setAmmoutSeconds: React.Dispatch<React.SetStateAction<number>>;
-  setCycles: React.Dispatch<React.SetStateAction<Cycle[]>>
-  setIsActive: React.Dispatch<React.SetStateAction<string | null>>;
+  dispatch: React.Dispatch<any>
   minMinuts:number
-  cycles:Cycle[]
+  cycles:Cycle[],
+  handInterruptinCycle:()=>void,
+  handFinishCycle:()=>void,
+  handleNewSubmitAction:(data:newTask)=>void
 }
 
 export const CyclesContext = createContext({} as CyclesContextType)
@@ -26,26 +29,121 @@ export const CyclesContext = createContext({} as CyclesContextType)
 interface CyclesContextProps {
   children:ReactNode
 }
+
+enum CycleActions {
+  ADD_NEW_CYCLE = "ADD_NEW_CYCLE",
+  STOP_CYCLE_ACTIVE = "STOP_CYCLE_ACTIVE",
+  FINISH_CYCLE= "FINISH_CYCLE"
+}
+
+interface AddNewCycleAction {
+  type: CycleActions.ADD_NEW_CYCLE;
+  payload: {
+    newCycle: Cycle;
+  };
+}
+
+interface stopCycleActive {
+  type:CycleActions.STOP_CYCLE_ACTIVE;
+
+}
+interface finishCycleActive {
+  type:CycleActions.FINISH_CYCLE;
+
+}
+type CycleAction = AddNewCycleAction | stopCycleActive | finishCycleActive
+
+interface CycleState {
+  cycles:Cycle[],
+  activeCycleId: string | null
+}
+
+
+
+
 export function CyclesContextProvider({ children }:CyclesContextProps) {
-    //states
-    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [cycleState, dispatch] = useReducer((state: CycleState, action: CycleAction) => {
+      switch (action.type) {
+        case CycleActions.ADD_NEW_CYCLE:
+          return {
+            ...state,
+            cycles:[...state.cycles,action.payload.newCycle],
+            activeCycleId: action.payload.newCycle.id
+          }
+          case CycleActions.STOP_CYCLE_ACTIVE:
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) =>
+              cycle.id === state.activeCycleId
+                ? { ...cycle, interruptDate: new Date() }
+                : cycle
+            ),
+            activeCycleId: null,
+          };
+          case CycleActions.FINISH_CYCLE:
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) =>
+              cycle.id === state.activeCycleId
+                ? { ...cycle, finishedDate: new Date() }
+                : cycle
+            ),
+            activeCycleId: null,
+          };
+        default:
+          return state;
+      }
+    }, {
+    cycles: [],
+    activeCycleId: null,
+    });
+    const {cycles, activeCycleId} = cycleState;
 
-    const [isActive, setIsActive] = useState<string | null>(null)
+
     const [ammoutSeconds , setAmmoutSeconds] = useState<number>(0)
-    //state
-    const activeCycle = cycles.find((cycles)=> cycles.id === isActive)
+    const activeCycle = cycles.find((cycles)=> cycles.id === activeCycleId)
     const minMinuts:number = 1
+    function handInterruptinCycle() {
 
+      if (activeCycleId !== null) {
+        dispatch({
+          type:CycleActions.STOP_CYCLE_ACTIVE
+        })
+      }
+    }
+    function handFinishCycle() {
+      dispatch({
+        type:CycleActions.FINISH_CYCLE
+      })
+    }
+
+    function handleNewSubmitAction(data:newTask) {
+      const id = String(new Date().getTime())
+      const newCycle :Cycle = {
+        id,
+        task:data.task,
+        minutsAmmout:data.minutsAmmout,
+        startDate:new Date(),
+      }
+      dispatch({
+       type:CycleActions.ADD_NEW_CYCLE,
+       payload:{
+         newCycle
+       }
+      })
+   }
   return(
     <CyclesContext.Provider value={{
       activeCycle,
-      isActive,
+      activeCycleId,
       ammoutSeconds,
       setAmmoutSeconds,
-      setCycles,
-      setIsActive,
+      dispatch,
       minMinuts,
-      cycles
+      cycles,
+      handInterruptinCycle,
+      handleNewSubmitAction,
+      handFinishCycle
       }}>
         {children}
       </CyclesContext.Provider>
--- vite-project/src/pages/Main/index.tsx
+++ vite-project/src/pages/Main/index.tsx
@@ -3,21 +3,22 @@ import { Hand, Play } from "@phosphor-icons/react";
 import {useContext} from "react";
 import CountDownComponent from "./countDown";
 import NewCycleForm from "./newCycleForm";
-import  {z}  from 'zod'
+import  {nan, z}  from 'zod'
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Cycle, CyclesContext } from "../../context/cyclesContext/cyclesContext";
 
+const newTaskSchemma = z.object({
+  task:z.string().min(1, 'Informe a tarefa'),
+  minutsAmmout:z.number().min(1).max(60)
+})
 
-
+export type newTask = z.infer<typeof newTaskSchemma>
 
 export default function Main() {
-  const {minMinuts, isActive, setIsActive,setCycles} = useContext(CyclesContext)
+  const {handleNewSubmitAction, dispatch,handInterruptinCycle, activeCycle} = useContext(CyclesContext)
 
-  const newTaskSchemma = z.object({
-    task:z.string().min(minMinuts, 'Informe a tarefa'),
-    minutsAmmout:z.number().min(1).max(60)
-  })
+
 
 
   const newCycleForm = useForm<newTask>({
@@ -27,7 +28,6 @@ export default function Main() {
       minutsAmmout:0
     }
   })
-type newTask = z.infer<typeof newTaskSchemma>
   const {handleSubmit, watch, reset} = newCycleForm
   //variaveis auxiliares
   const task = watch('task')
@@ -35,45 +35,17 @@ type newTask = z.infer<typeof newTaskSchemma>
   //
 
   function handleNewSubmit(data:newTask) {
-    const id = String(new Date().getTime())
-    const newCycle :Cycle = {
-      id,
-      task:data.task,
-      minutsAmmout:data.minutsAmmout,
-      startDate:new Date(),
-    }
-
-    setCycles((state)=>[...state,newCycle])
-    setIsActive(id)
-    reset()
+    handleNewSubmitAction(data)
   }
-  function handInterruptinCycle() {
 
-    if (isActive !== null) {
-      setCycles((state)=>
-      state.map((cycle) => {
-        if (cycle.id === isActive) {
-          return { ...cycle, interruptDate: new Date() };
-        } else {
-          return cycle;
-        }
-      }));
-    }
-
-    setIsActive(null);
-
-  }
  return (
     <HomeContainer>
         <FormProvider {...newCycleForm}>
           <form onSubmit={handleSubmit(handleNewSubmit)}>
-
             <NewCycleForm/>
             <CountDownComponent
-
             />
-
-            {isActive?(
+            {activeCycle?(
               <StopButton
                 onClick={handInterruptinCycle}
                 type="button">
